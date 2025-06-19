098fb51ff581aaebf35c3c75fc68acadcf811947
Roman Serdiuk <100283300+RomanSRDK@users.noreply.github.com>
2025-06-19 01:28:49 -0400

    Initial commit

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,33 @@
+import { FaUserAlt } from "react-icons/fa";
+import { FaPhoneAlt } from "react-icons/fa";
+import styles from "./Contact.module.css";
+import { useDispatch } from "react-redux";
+import { deleteContact } from "../../redux/contactsSlice";
+
+function Contact({ name, number, id }) {
+  const dispatch = useDispatch();
+  return (
+    <>
+      <div className={styles.contactWrapper}>
+        <div className={styles.row}>
+          <FaUserAlt className={styles.icon} />
+          <p className={styles.name}>{name}</p>
+        </div>
+        <div className={styles.row}>
+          <FaPhoneAlt className={styles.icon} />
+          <p className={styles.number}>{number}</p>
+        </div>
+      </div>
+      <button
+        className={styles.button}
+        onClick={() => {
+          dispatch(deleteContact(id));
+        }}
+      >
+        Delete
+      </button>
+    </>
+  );
+}
+
+export default Contact;
